69b764707679c42ad0f1fa0d2b50fd5a44fadd86
Michael Matthews <michael.64.matthews@gmail.com>
2024-02-06 01:46:16 -0500

    Update with more images, updated pals.json

---
+++ public/work-images/index.js
@@ -0,0 +1,16 @@
+export const workImages = {
+  'cooling':	require('./cooling.png'),
+  'farming':	require('./farming.png'),
+  'gathering':	require('./gathering.png'),
+  'generating_electricity':	require('./generating_electricity.png'),
+  'handiwork':	require('./handiwork.png'),
+  'kindling':	require('./kindling.png'),
+  'lumbering':	require('./lumbering.png'),
+  'medicine_production':	require('./medicine_production.png'),
+  'mining':	require('./mining.png'),
+  'planting':	require('./planting.png'),
+  'transporting':	require('./transporting.png'),
+  'watering':	require('./watering.png'),
+}
+
+export const hungerImage = require('./bread.png');
