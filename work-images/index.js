4893f5b349d6fd06497ea6fb7731e0a76e29b4ae
Michael Matthews <michael.64.matthews@gmail.com>
2024-02-07 02:18:42 -0500

    Updates

---
+++ work-images/index.js
@@ -0,0 +1,31 @@
+export const workImages = {
+  'cooling':	require('./cooling.png'),
+  'farming':	require('./farming.png'),
+  'gathering':	require('./gathering.png'),
+  'generating_electricity':	require('./generating_electricity.png'),
+  'handiwork':	require('./handiwork.png'),
+  'kindling':	require('./kindling.png'),
+  'lumbering':	require('./lumbering.png'),
+  'medicine_production':	require('./medicine_production.png'),
+  'mining':	require('./mining.png'),
+  'planting':	require('./planting.png'),
+  'transporting':	require('./transporting.png'),
+  'watering':	require('./watering.png'),
+}
+
+export const workImagesGray = {
+  'cooling':	require('./cooling-gray.png'),
+  'farming':	require('./farming-gray.png'),
+  'gathering':	require('./gathering-gray.png'),
+  'generating_electricity':	require('./generating_electricity-gray.png'),
+  'handiwork':	require('./handiwork-gray.png'),
+  'kindling':	require('./kindling-gray.png'),
+  'lumbering':	require('./lumbering-gray.png'),
+  'medicine_production':	require('./medicine_production-gray.png'),
+  'mining':	require('./mining-gray.png'),
+  'planting':	require('./planting-gray.png'),
+  'transporting':	require('./transporting-gray.png'),
+  'watering':	require('./watering-gray.png'),
+}
+
+export const hungerImage = require('./bread.png');
